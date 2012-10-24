85955f5c31f3bcf67eb36b56625f03b632cc7a37
Juan Carlos Carvajal <juancacr1999@gmail.com>
2012-10-24 13:21:04 -0600

    ActivitiController
    ActivitiController- Manages activiti engine
    Also adds activiti JARS

--- src/com/JqueryDemo/Logic/ActivitiController.java
+++ src/com/JqueryDemo/Logic/ActivitiController.java
@@ -1,10 +1,41 @@
 package com.JqueryDemo.Logic;
 
-public class ActivitiController {
+import java.io.PrintWriter;
 
-	public ActivitiController() {
-		String a="0";
-		// TODO Auto-generated constructor stub
+import java.io.IOException;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.runtime.ProcessInstance;
+
+public class ActivitiController {
+	ProcessEngine processEngine=null;
+	String process;
+
+	public ActivitiController(String _process) {
+		process=_process;
+		StartActivitiEngine();
+	}
+	
+	public void StartActivitiEngine(){
+		ProcessEngines.init();
+		processEngine = ProcessEngineConfiguration
+				.createStandaloneProcessEngineConfiguration()
+				.setJdbcUrl("jdbc:mysql://localhost:3306/psodemo")
+				.setJdbcDriver("com.mysql.jdbc.Driver").setJdbcUsername("root")
+				.setJdbcPassword("admin").setJobExecutorActivate(true)
+				.buildProcessEngine();
+		ProcessEngines.registerProcessEngine(processEngine);		
+	}
+	
+	public void CreateNewActivitiAprovalProcess(){
+		RepositoryService repositoryService = processEngine.getRepositoryService();
+		RuntimeService runtimeService = processEngine.getRuntimeService();
+		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(process);
 	}
 
 }
